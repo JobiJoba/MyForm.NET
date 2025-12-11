1848829ce05e27388eeaa07d07f87108fe4221c7
JobiJoba <6494791+JobiJoba@users.noreply.github.com>
2025-12-11 07:16:20 +0700

    Update for C#12 with primary constructor

--- MyForm.FormApi/Repositories/SimpleFormRepository.cs
+++ MyForm.FormApi/Repositories/SimpleFormRepository.cs
@@ -4,34 +4,27 @@ using MyForm.FormApi.Entities;
 
 namespace MyForm.FormApi.Repositories;
 
-public class SimpleFormRepository : ISimpleFormRepository
+public class SimpleFormRepository(MyFormDbContext context) : ISimpleFormRepository
 {
-    private readonly MyFormDbContext _context;
-
-    public SimpleFormRepository(MyFormDbContext context)
-    {
-        _context = context;
-    }
-
     public async Task<IEnumerable<SimpleForm>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        return await _context.Forms.ToListAsync(cancellationToken);
+        return await context.Forms.ToListAsync(cancellationToken);
     }
 
     public async Task<SimpleForm?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        return await _context.Forms.FindAsync([id], cancellationToken);
+        return await context.Forms.FindAsync([id], cancellationToken);
     }
 
     public async Task<SimpleForm> CreateAsync(SimpleForm form, CancellationToken cancellationToken = default)
     {
-        _context.Forms.Add(form);
-        await _context.SaveChangesAsync(cancellationToken);
+        context.Forms.Add(form);
+        await context.SaveChangesAsync(cancellationToken);
         return form;
     }
 
     public async Task<bool> ExistsAsync(int id, CancellationToken cancellationToken = default)
     {
-        return await _context.Forms.AnyAsync(f => f.Id == id, cancellationToken);
+        return await context.Forms.AnyAsync(f => f.Id == id, cancellationToken);
     }
 }
